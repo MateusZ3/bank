7a115a834bd5051677087f1cb746a1ead12bdf5c
Mateusz Zielinski <mateusz151994@wp.pl>
2016-02-05 05:43:44 +0100

    static

--- bd/settings.py
+++ bd/settings.py
@@ -122,8 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/bank/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
